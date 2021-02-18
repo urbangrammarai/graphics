4adc28035626a0033e8ac54ab5d3d1b9d60494c0
Martin Fleischmann <martin@martinfleischmann.net>
2021-02-18 11:47:30 +0000

    logo, font

--- setup.py
+++ setup.py
@@ -1,8 +1,5 @@
 import setuptools
 
-with open("README.md", "r", encoding="utf8") as fh:
-    long_description = fh.read()
-
 setuptools.setup(
     name="urbangrammar_graphics",
     version="1.1.0",
@@ -11,6 +8,5 @@ setuptools.setup(
     python_requires=">=3.6",
     install_requires=["matplotlib", "seaborn", "numpy", "contextily"],
     description="Visual style for Urban Grammar AI research project",
-    long_description=long_description,
     url="https://github.com/urbangrammarai/graphics",
 )
