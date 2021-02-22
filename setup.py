aa47855e305265ef3a0da6d9ee4f00bf4d9ba41d
Martin Fleischmann <martin@martinfleischmann.net>
2021-02-22 19:11:09 +0000

    fix pip

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf8") as fh:
 
 setuptools.setup(
     name="urbangrammar_graphics",
-    version="1.2.0",
+    version="1.2.1",
     author="Martin Fleischmann",
     author_email="martin@martinfleischmann.net",
     python_requires=">=3.6",
@@ -14,4 +14,5 @@ setuptools.setup(
     url="https://github.com/urbangrammarai/graphics",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
 )
